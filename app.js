e2adc3bb625e84bf4673819edaf8c22667c6349e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-21 21:30:57 +0600

    settings for chat-application

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+ * Title : Chat Application
+ * Description : Realtime Node.Js Chat Application with Websocket.io and Express.Js
+ * Author : Kishor Paroi
+ * Date : 2023/11/21
+ * Time : 9:27:44 PM
+ */
+
