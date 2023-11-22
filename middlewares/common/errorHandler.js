2b4d45b3fc503d2417fe815f00eb057ed3ca778e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-22 20:49:48 +0600

    errorhandler for showing response in both html and json

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -8,9 +8,22 @@
 
 // default error handler
 function errorHandler(err, req, res, next) {
-  res.render("error", {
-    title: "Error Page",
-  });
+  //   res.locals.title = "Error Page";
+  //   res.render("error");
+
+  res.locals.error =
+    process.env.NODE_ENV === "development" ? err : { message: err.message };
+  res.status(err.status || 500);
+
+  if (!res.locals.html) {
+    // html response
+    res.render("error", {
+      title: "Error Page",
+    });
+  } else {
+    // json response
+    res.json(res.locals.error);
+  }
 }
 
 export default errorHandler;
